261f6e57993b642bd053ca00dfc713c8a5da5958
sagar <sagar.makwana@tatvasoft.com>
2025-07-24 11:24:59 +0530

    Update buildspec file to push docker image to ecr

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "Starting Docker Compose deployment..."
 
-export RDS_HOSTNAME=database-1.cud4ao0gqt8o.us-east-1.rds.amazonaws.com
-export RDS_DB_NAME=my_db
-export RDS_USERNAME=postgres
-export RDS_PASSWORD=SagarMakwana
+export RDS_HOSTNAME=
+export RDS_DB_NAME=
+export RDS_USERNAME=
+export RDS_PASSWORD=
 export RDS_PORT=5432
 
 cd /home/ec2-user/app || exit 1
