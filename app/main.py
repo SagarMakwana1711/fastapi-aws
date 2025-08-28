9ec37cfd96cc60f6ade957abfac9928f4d6fb57b
sagar <sagar.makwana@tatvasoft.com>
2025-08-28 12:21:22 +0530

    fix cors error

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-from . config.database import get_db
+
+from .config.database import get_db
 from app.routers import router
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def index(db: Session = Depends(get_db)):
-    return { "message" : "Welcome to the fastapi after the deployment." }
+    return {"message": "Welcome to the fastapi after cors."}
 
 app.include_router(router)
